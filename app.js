4b6855fb92b69660b7a83f49a4ea916e5438d4c9
lhumeau <luishumeau90@gmail.com>
2024-04-01 16:50:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const ActiveDirectory = require('activedirectory2');
 const fs = require('fs').promises; // Importa fs.promises para trabajar con archivos de forma asincrónica
 const config = {
-  url: 'ldap://domainController',
+  url: 'ldap://your-domain-controller',
   baseDN: 'dc=domain,dc=local',
 };
 
@@ -10,7 +10,7 @@ function authenticate(username, password) {
     const ad = new ActiveDirectory({ ...config, username, password });
     ad.authenticate(username, password, function (err, auth) {
       if (err) {
-        resolve({ username, success: false, error: err }); // Usamos resolve para manejar los errores
+        resolve({ username, success: false, error: err });
         return;
       }
       resolve({ username, success: auth });
@@ -18,37 +18,53 @@ function authenticate(username, password) {
   });
 }
 
-const users = [
-  { username: 'user1@domain.local', password: 'password1' },
-  { username: 'user2@domain.local', password: 'password2' },
-  // Añade más usuarios según sea necesario
-];
+async function readUsers() {
+  try {
+    const data = await fs.readFile('users.json', 'utf8');
+    const users = JSON.parse(data);
+    if (users && users.length > 0) {
+      return users;
+    } else {
+      throw new Error('El archivo users.json está vacío.');
+    }
+  } catch (error) {
+    console.log('Usando usuarios por defecto debido a: ', error.message);
+    return [
+      { username: 'user1@domain.local', password: 'password1' },
+      { username: 'user2@domain.local', password: 'password2' },
+    ];
+  }
+}
 
-const promises = users.map((user) =>
-  authenticate(user.username, user.password)
-);
+async function main() {
+  const users = await readUsers();
+  const promises = [];
 
-Promise.allSettled(promises).then(async (results) => {
-  // Prepara el texto para escribir en el archivo basado en los resultados
-  const output = results
-    .map((result) => {
+  for (const user of users) {
+    promises.push(authenticate(user.username, user.password));
+  }
+
+  Promise.allSettled(promises).then(async (results) => {
+    let output = '';
+    for (const result of results) {
       if (result.status === 'fulfilled' && result.value.success) {
-        return `Autenticación exitosa: ${result.value.username}\n`;
+        output += `Autenticación exitosa: ${result.value.username}\n`;
       } else if (result.status === 'fulfilled') {
-        return `Autenticación fallida: ${
+        output += `Autenticación fallida: ${
           result.value.username
         }, Error: ${JSON.stringify(result.value.error)}\n`;
       }
-    })
-    .join('');
+    }
 
-  // Escribe los resultados en 'result.txt', reemplazándolo si ya existe
-  try {
-    await fs.writeFile('result.txt', output);
-    console.log(
-      'Los resultados de la autenticación se han guardado en "result.txt".'
-    );
-  } catch (error) {
-    console.error('Error al escribir los resultados en el archivo:', error);
-  }
-});
+    try {
+      await fs.writeFile('result.txt', output);
+      console.log(
+        'Los resultados de la autenticación se han guardado en "result.txt".'
+      );
+    } catch (error) {
+      console.error('Error al escribir los resultados en el archivo:', error);
+    }
+  });
+}
+
+main();
