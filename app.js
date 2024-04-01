5c97032d2ccb8e7da90ddcb6c1386c73c09170cc
lhumeau <luishumeau90@gmail.com>
2024-04-01 16:39:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const ActiveDirectory = require('activedirectory2');
+const fs = require('fs').promises; // Importa fs.promises para trabajar con archivos de forma asincrónica
 const config = {
-  url: 'ldap://ADSERVER',
-  baseDN: 'dc=yourdomain,dc=local',
+  url: 'ldap://domainController',
+  baseDN: 'dc=domain,dc=local',
 };
 
 function authenticate(username, password) {
   return new Promise((resolve, reject) => {
     const ad = new ActiveDirectory({ ...config, username, password });
-    ad.authenticate(username, password, function(err, auth) {
+    ad.authenticate(username, password, function (err, auth) {
       if (err) {
-        resolve({ username, success: false, error: err }); // Usamos resolve para manejar errores individualmente
+        resolve({ username, success: false, error: err }); // Usamos resolve para manejar los errores
         return;
       }
       resolve({ username, success: auth });
@@ -18,18 +19,36 @@ function authenticate(username, password) {
 }
 
 const users = [
-  { username: 'user1@yourdomain.local', password: 'password1' },
-  { username: 'user2@yourdomain.local', password: 'password2' },
-  // Añade más usuarios según necesario
+  { username: 'user1@domain.local', password: 'password1' },
+  { username: 'user2@domain.local', password: 'password2' },
+  // Añade más usuarios según sea necesario
 ];
 
-Promise.allSettled(users.map(user => authenticate(user.username, user.password)))
-  .then(results => {
-    results.forEach(result => {
+const promises = users.map((user) =>
+  authenticate(user.username, user.password)
+);
+
+Promise.allSettled(promises).then(async (results) => {
+  // Prepara el texto para escribir en el archivo basado en los resultados
+  const output = results
+    .map((result) => {
       if (result.status === 'fulfilled' && result.value.success) {
-        console.log(`Autenticación exitosa: ${result.value.username}`);
+        return `Autenticación exitosa: ${result.value.username}\n`;
       } else if (result.status === 'fulfilled') {
-        console.log(`Autenticación fallida: ${result.value.username}, Error: ${result.value.error}`);
+        return `Autenticación fallida: ${
+          result.value.username
+        }, Error: ${JSON.stringify(result.value.error)}\n`;
       }
-    });
-  });
+    })
+    .join('');
+
+  // Escribe los resultados en 'result.txt', reemplazándolo si ya existe
+  try {
+    await fs.writeFile('result.txt', output);
+    console.log(
+      'Los resultados de la autenticación se han guardado en "result.txt".'
+    );
+  } catch (error) {
+    console.error('Error al escribir los resultados en el archivo:', error);
+  }
+});
