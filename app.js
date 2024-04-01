653aad3c2455ff67e285b5c3ff109117d029112c
lhumeau <luishumeau90@gmail.com>
2024-04-01 16:16:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const ActiveDirectory = require('activedirectory2');
 const config = {
-  url: 'ldap://192.168.11.2', // Dirección IP del servidor AD
-  baseDN: 'dc=rizek,dc=local', // DN base de tu dominio
-  username: 'usuario@rizek.local', // Un usuario con permisos para buscar en el AD
-  password: 'tuContraseñaAdmin' // Contraseña del usuario
+  url: 'ldap://tu.dominio.com',
+  baseDN: 'dc=tu,dc=dominio,dc=com',
+  username: 'usuario@tu.dominio.com',
+  password: 'tuContraseñaAdmin'
 };
 
 const ad = new ActiveDirectory(config);
-const username = 'usuario@rizek.local'; // El usuario que quieres verificar
+const username = 'usuario@tu.dominio.com'; // El usuario que quieres verificar
 const password = 'contraseñaUsuario'; // La contraseña que quieres verificar
 
 // Intenta autenticar al usuario
