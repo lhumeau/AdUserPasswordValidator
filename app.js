4103f765ae71a145ec51147e61a46cfa5e8ff33d
lhumeau <luishumeau90@gmail.com>
2024-04-01 16:28:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 const ActiveDirectory = require('activedirectory2');
 const config = {
-  url: 'ldap://tu.dominio.com',
-  baseDN: 'dc=tu,dc=dominio,dc=com',
-  username: 'usuario@tu.dominio.com',
-  password: 'tuContraseñaAdmin'
+  url: 'ldap://ADSERVER',
+  baseDN: 'dc=yourdomain,dc=local',
 };
 
-const ad = new ActiveDirectory(config);
-const username = 'usuario@tu.dominio.com'; // El usuario que quieres verificar
-const password = 'contraseñaUsuario'; // La contraseña que quieres verificar
+function authenticate(username, password) {
+  return new Promise((resolve, reject) => {
+    const ad = new ActiveDirectory({ ...config, username, password });
+    ad.authenticate(username, password, function(err, auth) {
+      if (err) {
+        resolve({ username, success: false, error: err }); // Usamos resolve para manejar errores individualmente
+        return;
+      }
+      resolve({ username, success: auth });
+    });
+  });
+}
 
-// Intenta autenticar al usuario
-ad.authenticate(username, password, function(err, auth) {
-  if (err) {
-    console.log('ERROR: ' + JSON.stringify(err));
-    return;
-  }
+const users = [
+  { username: 'user1@yourdomain.local', password: 'password1' },
+  { username: 'user2@yourdomain.local', password: 'password2' },
+  // Añade más usuarios según necesario
+];
 
-  if (auth) {
-    console.log('Autenticado con éxito!');
-  }
-  else {
-    console.log('Autenticación fallida!');
-  }
-});
+Promise.allSettled(users.map(user => authenticate(user.username, user.password)))
+  .then(results => {
+    results.forEach(result => {
+      if (result.status === 'fulfilled' && result.value.success) {
+        console.log(`Autenticación exitosa: ${result.value.username}`);
+      } else if (result.status === 'fulfilled') {
+        console.log(`Autenticación fallida: ${result.value.username}, Error: ${result.value.error}`);
+      }
+    });
+  });
